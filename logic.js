b9765541072ae8c5d7d63df0841bf61c1b10081e
Grascon <gilberto.r.s@icloud.com>
2019-03-12 22:04:38 -0700

    Added styling and transitions

--- logic.js
+++ logic.js
@@ -1,10 +1,65 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
   
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+
+easyBtn.addEventListener("click", function(){
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (var i = 0; i < squares.length; i++){
+       if (colors[i]){
+           squares[i].style.backgroundColor = colors[i];
+       }
+       else {
+        squares[i].style.display = "none";
+       }
+    }
+    h1.style.background = "steelblue";
+});
+
+hardBtn.addEventListener("click", function(){
+    easyBtn.classList.remove("selected");
+    hardBtn.classList.add("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    colors = generateRandomColors(6);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+
+    };
+    h1.style.background = "steelblue";
+});
+
+resetButton.addEventListener("click", function(){
+    //generate all new colors
+    colors = generateRandomColors(numSquares);
+    //pick new random colors
+    pickedColor = pickColor();
+    //change color display
+    colorDisplay.textContent = pickedColor;
+    //change colors of squares
+    for (var i = 0; i < squares.length; i++){
+        //add colors
+        squares[i].style.backgroundColor = colors[i];
+        //add click listeners
+    }
+    h1.style.background = "steelblue";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -20,6 +75,7 @@ for (var i = 0; i < squares.length; i++){
       //compare color
       if (clickedColor === pickedColor){
         messageDisplay.textContent = "Correct!";
+        resetButton.textContent = "Play Again?"
         changeColors(clickedColor);
         h1.style.background = clickedColor;
       }
